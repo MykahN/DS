2e415e77a634df58681c3645032679c9650d9fa2
MykahN <89401136+MykahN@users.noreply.github.com>
2021-09-15 09:54:50 -0400

    Added randomuser API functionality

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,26 +1,40 @@
 const Offer = {
     data() {
         return {
-    "offers": [
-        {
-            "id": 1,
-            "name": "Mykah Netzky",
-            "salary": 125000,
-            "bonus": 10000,
-            "company": "EY",
-            "offerDate": "2021-10-12"
-        },
-        {
-            "id": 2,
-            "name": "Abhi Sridhar",
-            "salary": 65000,
-            "bonus": 1000,
-            "company": "IU",
-            "offerDate": "2021-10-12"
+            "result": {},
+            "offers": [
+                {
+                    "id": 1,
+                    "name": "Mykah Netzky",
+                    "salary": 125000,
+                    "bonus": 10000,
+                    "company": "EY",
+                    "offerDate": "2021-10-12"
+                },
+
+                {
+                    "id": 2,
+                    "name": "Abhi Sridhar",
+                    "salary": 65000,
+                    "bonus": 1000,
+                    "company": "IU",
+                    "offerDate": "2021-10-12"
+                }
+            ]
         }
-    ]
-}
-    }
+    },
+
+    created() {
+        fetch('https://randomuser.me/api')
+        .then(response => response.json())
+        .then((json) => {
+            console.log("Got json back:", json);
+            this.result = json.results[0];
+        })
+        .catch( (error) => {
+            console.error(error);
+        });
+    },
 }
 
 Vue.createApp(Offer).mount('#offerApp')
